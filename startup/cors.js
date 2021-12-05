b8b0c0fd71a18f46645343f57b8eba165bcf9c7a
AFLP2199 <fernandopiceda99@gmail.com9>
2021-12-05 19:08:52 -0300

    Modify CORS

--- startup/cors.js
+++ startup/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 module.exports = function (app) {
     app.use(
         cors({
-            origin: ["https://afternoon-caverns-13473.herokuapp.com", "http://localhost:3000"],
+            origin: "*",
         })
     );
 };
