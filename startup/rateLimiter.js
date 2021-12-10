f2e0d5e3f59e4e3b0eda27b7f7b0e530d83b77d5
AFLP2199 <fernandopiceda99@gmail.com9>
2021-12-10 00:04:56 -0300

    Update rate limiter

--- startup/rateLimiter.js
+++ startup/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 3, // limit each IP to 100 requests per windowMs
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 10, // limit each IP to 100 requests per windowMs
 });
 
 module.exports = function (app) {
