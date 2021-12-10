59e335649d62b1c58edde7e09cf0ba336156b870
AFLP2199 <fernandopiceda99@gmail.com9>
2021-12-10 00:09:53 -0300

    Update rate limiter

--- startup/rateLimiter.js
+++ startup/rateLimiter.js
@@ -9,5 +9,5 @@ module.exports = function (app) {
     // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB or API Gateway, Nginx, etc)
     // see https://expressjs.com/en/guide/behind-proxies.html
     app.set("trust proxy", 1);
-    app.use(limiter);
+    app.use("/api/users", limiter);
 };
